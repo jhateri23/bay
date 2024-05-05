364848bd64bdb315b12d0fd40e476f6f1a7ef3b1
jhateri23 <109025944+jhateri23@users.noreply.github.com>
2024-05-05 19:17:18 +0530

    Add files via upload

---
+++ dep.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+# Streamlit app
+def main():
+    st.title("Basic Streamlit App")
+    st.header("Welcome to Streamlit!")
+    st.write("This is a basic Streamlit app.")
+
+if __name__ == "__main__":
+    main()
